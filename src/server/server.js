6c1debc42d27d52a2b16067719b24890535df5a3
raziel214 <raziel214@users.noreply.github.com>
2021-08-14 09:39:03 -0500

    ajuste del chunk

--- src/server/server.js
+++ src/server/server.js
@@ -47,7 +47,7 @@ if (ENV === 'development') {
 const setResponse = (html, preloadedState, manifest) => {
   const mainStyles = manifest ? manifest['main.css'] : 'assets/app.css';
   const mainBuild = manifest ? manifest['main.js'] : 'assets/app.js';
-  const mainBuild = manifest ? manifest['vendors.js'] : 'assets/vendor.js';
+  const vendorBuild = manifest ? manifest['vendors.js'] : 'assets/vendor.js';
   return (`
   <!DOCTYPE html>
   <html>
@@ -61,6 +61,7 @@ const setResponse = (html, preloadedState, manifest) => {
         window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}
       </script>
       <script src="${mainBuild}" type="text/javascript"></script>
+      <script src="${vendorBuild}" type="text/javascript"></script>
     </body>
   </html>
   `);
